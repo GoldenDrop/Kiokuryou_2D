f71c4f1adc4726331dbfc864205d20e6a481d933
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-05-29 00:16:53 +0900

    TitleからPlayerがタッチするまでの遷移
    暗幕の移動や、覚えるために一定時間待つのにコルーチンを使用する

---
+++ 2D Kiokuryoku/Assets/Scrips/Test/CoroutineTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineTest : MonoBehaviour {
+
+	void Start () {
+        StartCoroutine("Test");
+	    
+	}
+
+    IEnumerator Test()
+    {
+        Debug.Log("Test");
+
+        yield return new WaitForSeconds(3.0f);
+        Debug.Log("Test 1");
+    }
+}
